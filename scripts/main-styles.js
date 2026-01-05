cd9e30b0c4f61593daa34f205831a4e3421e8764
ZennOnline <98044741+ZennOnline@users.noreply.github.com>
2026-01-05 19:07:01 +0100

    main js

---
+++ scripts/main-styles.js
@@ -0,0 +1,5 @@
+ // Header click = go back
+    document.getElementById('header-title').addEventListener('click', () => {
+      window.location.href = '../index.html';
+});
+
--- scripts/projects-basics.js
+++ scripts/projects-basics.js
@@ -1,8 +1,3 @@
-// Header click = go back
-    document.getElementById('header-title').addEventListener('click', () => {
-      window.location.href = '../index.html';
-});
-
 // Tabs functionality
     const tabs = document.querySelectorAll('.tab');
     const contents = document.querySelectorAll('.tab-content');
